04ef66573850b41248b425d0e83fce1b067d3a73
Borza Zsolt <borza.zsolt89@gmail.com>
2024-03-23 17:56:01 +0100

    adding modules, change sizes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,18 @@ import { NavbarComponent } from './core/navbar/navbar.component';
 import { NbaPredictionsComponent } from './components/nba-predictions/nba-predictions.component';
 import { MlbPredictionsComponent } from './components/mlb-predictions/mlb-predictions.component';
 import { PredictionService } from './service/prediction.service';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { HttpClientJsonpModule, HttpClientModule} from "@angular/common/http";
 import { PredictionCardComponent } from './components/prediction-card/prediction-card.component';
 import { FooterComponent } from './core/footer/footer.component';
 import { JumbotronComponent } from './core/jumbotron/jumbotron.component';
+import {MatOptionModule} from "@angular/material/core";
+import {FormsModule} from "@angular/forms";
+import { ForbiddenComponent } from './page/forbidden/forbidden.component';
 
 @NgModule({
   declarations: [
@@ -22,15 +30,23 @@ import { JumbotronComponent } from './core/jumbotron/jumbotron.component';
     MlbPredictionsComponent,
     PredictionCardComponent,
     FooterComponent,
-    JumbotronComponent
+    JumbotronComponent,
+    ForbiddenComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     HttpClientJsonpModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule,
     NgbModule,
     NgbCollapseModule,
+    MatOptionModule,
+    FormsModule,
+    MatSelectModule,
 
   ],
   providers: [],
